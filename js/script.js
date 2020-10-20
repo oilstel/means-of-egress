ccae14058db127da821006c2a281cbc57977ca56
Elliott <elliottcost@gmail.com>
2020-10-20 13:48:04 -0400

    comments

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ let button = document.querySelector('#start-video');
 let desktop = document.querySelector('.desktop');
 
 function startVideo() {
-    desktop.style.visibility = 'hidden';
-    video_container.style.visibility = 'visible';
-    video.play();
+  desktop.style.visibility = 'hidden';
+  video_container.style.visibility = 'visible';
+  video.play();
 }
 
 // On button click start video
@@ -14,18 +14,18 @@ button.addEventListener('click', () => startVideo());
 
 // Loop when video ends
 video.addEventListener('ended', e => {
-    console.log(event.target.currentTime);
-    e.target.currentTime = 3;
-    e.target.muted = false;
-    e.target.play();
+  console.log(event.target.currentTime);
+  e.target.currentTime = 3;
+  e.target.muted = false;
+  e.target.play();
 });
 
 // When video is clicked reset playback and hide
 video.addEventListener('click', e => {
-    e.target.pause();
-    e.target.currentTime = 0;
-    video_container.style.visibility = 'hidden';
-    desktop.style.visibility = 'visible';
+  e.target.pause();
+  e.target.currentTime = 0;
+  video_container.style.visibility = 'hidden';
+  desktop.style.visibility = 'visible';
 });
 
 // Button hover states
